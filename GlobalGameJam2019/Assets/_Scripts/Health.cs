8a05ad4fb39fcdfa95446469b26c91b5e0ed71ff
Kathlar <thomass1@wp.pl>
2019-01-17 21:10:30 +0100

    HealthBar

--- GlobalGameJam2019/Assets/_Scripts/Health.cs
+++ GlobalGameJam2019/Assets/_Scripts/Health.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    public Image healthBar;
+    protected RectTransform healthBarTransform;
+
     public int maxHealth = 50;
     protected int currentHealth;
 
@@ -11,15 +15,26 @@ public class Health : MonoBehaviour
 
     protected int numberOfDeaths;
 
+    void Awake()
+    {
+        healthBarTransform = healthBar.transform.parent.GetComponent<RectTransform>();
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
         startPosition = transform.position;
     }
+
+    void LateUpdate()
+    {
+        healthBarTransform.position = Camera.main.WorldToScreenPoint(transform.position + transform.up * 2);
+    }
     
     public void GetDamage(int value)
     {
         currentHealth = Mathf.Clamp(currentHealth - value, 0, currentHealth);
+        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
         if (currentHealth <= 0)
         {
             Die();
