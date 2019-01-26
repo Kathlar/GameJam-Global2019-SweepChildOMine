da3d22898ab8cda920ac405809889d3586d3a127
epss77 <46776419+epss77@users.noreply.github.com>
2019-01-26 20:16:03 +0100

    wash

--- GlobalGameJam19/Assets/_Scripts/Interactives/WashingMachine.cs
+++ GlobalGameJam19/Assets/_Scripts/Interactives/WashingMachine.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class WashingMachine : Shelf {
 
 
+    protected override void OnTriggerEnter(Collider other)
+    {
+        ItemObject item = other.GetComponent<ItemObject>();
+        base.OnTriggerEnter(other);
+        if (itemOn != null)
+        {
+            if (itemOn.objectType == ItemObjectType.Cloth && item.status == 0)
+                itemOn.enabled = false;
+            else
+                item.transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+    }
 }
