de5e4ef5668ff86bab152ad78c7995f1adbb7526
epss77 <46776419+epss77@users.noreply.github.com>
2019-01-26 22:01:26 +0100

    animation

--- GlobalGameJam19/Assets/_Scripts/Interactives/WashingMachine.cs
+++ GlobalGameJam19/Assets/_Scripts/Interactives/WashingMachine.cs
@@ -6,6 +6,8 @@ public class WashingMachine : Shelf {
 
     int currentCapacity = 0;
     public GameObject window;
+    public Animator animator;
+    Quaternion rotaion;
 
     protected override void OnTriggerEnter(Collider other)
     {
@@ -20,11 +22,13 @@ public class WashingMachine : Shelf {
             if (itemOn.objectType == ItemObjectType.Cloth && item.status == 0)
             {
                 itemOn.enabled = false;
+                rotaion = transform.rotation;
                 Destroy(item.gameObject);
                 currentCapacity += 1;
                 if(currentCapacity == 1)
                     window.GetComponent<MeshRenderer>().enabled = true;
                 Invoke("OpenWaschingMachine", 5f);
+                animator.SetBool("waschingMachine", true);
             }
             else
                 item.transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
@@ -34,5 +38,7 @@ public class WashingMachine : Shelf {
     private void OpenWaschingMachine()
     {
         window.GetComponent<MeshRenderer>().enabled = false;
+        animator.enabled = false;
+        transform.rotation = rotaion;
     }
 }
