b451d30fd770dae970479d94973681b2ac7a8609
kathlar <tomkathlar@gmail.com>
2019-01-26 15:32:58 +0100

    Main Menu music

--- GlobalGameJam19/Assets/_Scripts/MainMenu.cs
+++ GlobalGameJam19/Assets/_Scripts/MainMenu.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-
-public void PlayGame ()
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame ()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
